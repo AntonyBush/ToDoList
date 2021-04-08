5e3a655a55147ffc7b1b0e61ee6c6c1960f63912
AntonyBush <antonybush95@gmail.com>
2021-04-08 15:45:18 +0530

    Debug=False

--- todolist/settings.py
+++ todolist/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '&^fuh-%)ws&-a=9tzojb+j9imb68220n(&f)xkzl)mqx7pk%1&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
